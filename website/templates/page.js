28fd2a557b6c15a245cb53e15a3073fd1e07acae
cda94581 <cda94581@gmail.com>
2021-04-03 23:16:54 -0500

    Embeds (maybe)

--- website/templates/page.js
+++ website/templates/page.js
@@ -6,6 +6,13 @@ function write(levels, pageTitle) {
 		levelCount++;
 	}
 	document.write('\
+		</body> \
+		<head> \
+			<meta property="og:title" content="' + pageTitle + ' | cda94581\'s Website" />\
+			<meta property="og:description" content="cda94581, hobby YouTuber, Programmer, Add-on Developer; Technical Minecraft Know-how, Minecraft Let\'s Plays, and more. />\
+			<meta property="og:image" content="' + pathBack + '../resources/favicon.ico" />\
+		</head> \
+		<body> \
 		<link rel="stylesheet" href="' + pathBack + '../styles/page.css"> \
 		<title>' + pageTitle + ' | cda94581\'s Website</title> \
 		<link rel="stylesheet" href="' + pathBack + '../styles/navigation.css"> \
